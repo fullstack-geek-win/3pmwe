f158783b52cab4f964a282be939a78d16cf18cd8
fullstack-geek-win <134689261+fullstack-geek-win@users.noreply.github.com>
2023-08-13 17:56:11 +0530

    Javascript Introduction, Error Handling
    Javascript Introduction, Error Handling

---
+++ Ex_07_javascript/js/script.js
@@ -0,0 +1,140 @@
+// let i; // variable declaration
+// i = 10; // value assignment into the variable
+// let j3 = 14;
+// console.log(i);
+
+// i = 1.2342342342342;
+// let j = i;
+// console.log(j);
+
+// Scope
+// =====
+// var
+// let
+// const
+
+// a = "balaji";
+// b = 20;
+// c = a + b;
+// console.log(c);
+
+// let k = 10;
+// // == equal
+// // === all are equal
+// if (k == "10") {
+//     console.log("True");
+//     console.log("True");
+//     console.log("True");
+// }
+// else {
+//     console.log("False");
+//     console.log("False");
+//     console.log("False");
+//     console.log("False");
+//     console.log("False");
+// }
+
+// For - looping statement
+// =======================
+// let i = 1;
+// var i = 2;
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i + " False");
+// }
+// console.log(i + " False");
+
+// const pi = 1.414;
+// const a = "alpha a";
+// console.log(pi);
+
+// // pi = 2324;
+// console.log(pi * 34);
+
+// Ternary Operator
+// ================
+
+// condition ? exprIfTrue : exprIfFalse;
+
+// Increament & Decrement Operator
+// ===============================
+
+// let i = 10;
+// console.log(i);
+// i = i + 1;
+// i++;
+// i += 1;
+
+// i = i - 1;
+// console.log(i);
+// i--;
+// console.log(i);
+// i -= 1;
+// console.log(i);
+
+// i++ 
+// i--
+
+// let i = 10;
+// i = i % 3
+// console.log(i);
+// i = i*2
+
+// i*=1
+
+// i = i/2
+
+// i/=1
+
+
+
+// i%=1
+
+// console.log(i++);
+// console.log(i);
+
+
+
+// ++i; //increase and assign
+// i++; //assign and increase
+
+// +
+// -
+// *
+// /
+// %
+
+// let i = 10;
+// i = i * 5;
+// i*=5;
+
+// let j = 1;
+// for (let i = 7; i <= 70; i += 7) {
+//     console.log("7 * " + j + " = " + i);
+//     j++;
+// }
+
+let k = 1;
+for (let i = 1; i <= 10; i++) {
+    console.log("11 * " + k + " = " + (i * 11));
+    k++;
+}
+
+// case 1:
+//     Identify prime nos between 1 to 100
+// 17
+// 1 17 - prime
+// 16
+// 1 2 4 16
+ 
+// case 1:
+//     Identify square root nos between 1 to 100
+// 1
+// 4
+// 9
+// 16
+// 25
+// 36
+// 49
+// 64
+// 81
+// 100
